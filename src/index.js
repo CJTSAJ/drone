0ed646721d7ad9bebe3e3dbb5063ca3d4c312f70
CJTSAJ <997149731@qq.com>
2019-01-06 23:55:28 +0800

    test6

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
-// Render the main component into the dom sefesfasdad  asdsaasdasdsdsa
+// Render the main component into the dom sefesfasdad  asdsaasdasdsdsadcdsc
 ReactDOM.render(<App />, document.getElementById('app'));
