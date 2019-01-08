72d9b3cdcdd9fb66af00a1d83f24817495469084
CJTSAJ <997149731@qq.com>
2019-01-09 01:50:18 +0800

    test

--- Dockerfile
+++ Dockerfile
@@ -3,3 +3,5 @@ FROM nginx:latest
 ADD ./dist /usr/share/nginx/html
 
 COPY nginx.conf /etc/nginx/conf.d/default.conf
+
+RUN /bin/bash -c 'echo init ok!!!'
---
+++ src/components/demo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <input type="button" @click="test" value="按钮">
+    <text>{{msg}}</text>
+  </div>
+</template>
+
+
+<script>
+import $ from 'jquery'
+export default {
+  name: 'demo',
+  data () {
+    return {
+      msg:'点击按钮'
+    }
+  },
+  methods:{
+    test:function(){
+    console.log("dsa")
+      $.get("http://localhost:8075"+"/test2",(response)=>{
+        console.log("asd")
+        this.msg=response
+      })
+    }
+  }
+}
+</script>
